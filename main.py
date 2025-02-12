cbd7b69bf701c4cfc0a6a425927aa0fb94b54efb
Natix <96585803+Natix1@users.noreply.github.com>
2025-02-12 20:32:43 +0100

    Added support for wildcard; significantly populated automation.json

--- main.py
+++ main.py
@@ -15,11 +15,24 @@ if not API_KEY:
 else:
     log.debug("Found OPEN_CLOUD_KEY environment variable")
 
+log.info("GDPR Obliterator by natix1")
+log.info("Since you are reading this file, you are: ")
+log.info("a) Curious")
+log.info("b) Debugging")
+log.info("If it is the second option, I'm sorry! Please direct any errors to Issues or DM me on discord: natix1, might be curious and check that out")
+
 automateList = None
+
+ExperiencePool = {}
+DatastorePool = {}
+
 with open("automation.json", "r") as f:
     automateList = json.load(f)
 
-UID = 1862042823
+UID = int(input("Enter user ID to purge: "))
+if not UID:
+    log.error("No UID entered")
+    exit(1)
 
 def confirmDestructiveAction():
     print("The above action is destructive, meaning it has irreversible consequences. Are you 100% certain? y(es)/n(o)", end=" ")
@@ -39,28 +52,75 @@ def confirmDestructiveAction():
         log.critical("Invalid choice: " + choice)
         exit(1)
 
-def clearKey(UniverseID: int, dataStoreName: str, keyName: str):
-    Experience = rbx.Experience(UniverseID, API_KEY)
-    Datastore = Experience.get_datastore(dataStoreName)
+def clearKey(UniverseID: int, dataStoreName: str, keyName: str) -> int:
+    clearedKeys = 0
+    Experience: rbx.Experience = None
+    if UniverseID in ExperiencePool:
+        Experience = ExperiencePool[UniverseID]
+    else:
+        Experience = rbx.Experience(UniverseID, API_KEY)
+        ExperiencePool[UniverseID] = Experience
+    
+    Datastore: rbx.datastore = None
+    formatForStore = f"{dataStoreName}_{UniverseID}"
+    if formatForStore in DatastorePool:
+        Datastore = DatastorePool[formatForStore]
+    else:
+        Datastore = Experience.get_datastore(dataStoreName)
+        DatastorePool[formatForStore] = Datastore
+
     if not Datastore:
         log.warning(f"Invalid clearKey call, skipping key {keyName} for datastore {dataStoreName} in Universe {UniverseID}")
-        return
+        return clearedKeys
     
     log.info(f"Clearing key {keyName} in datastore {dataStoreName} in universe {UniverseID}")
-    Datastore.remove_entry(keyName)
+    
+    if "*" in keyName:
+        prefix = keyName.split("*")[0]
+        log.info(f"Wildcard detected, listing keys with prefix: {prefix}")
+        keys = Datastore.list_keys(prefix)
+        
+        for key in keys:
+            log.info(f"Clearing key {key} in datastore {dataStoreName} in universe {UniverseID}")
+            try:
+                Datastore.remove_entry(key)
+                log.info(f"Clearing key {key} in datastore {dataStoreName} in universe {UniverseID}")
+                clearedKeys += 1
+            except rbx.exceptions.NotFound:
+                log.info(f"Tried deleting invalid key (this is normal): {key}")
+        
+    else:
+        log.info(f"Clearing key {keyName} in datastore {dataStoreName} in universe {UniverseID}")
+        try:
+            Datastore.remove_entry(keyName)
+            clearedKeys += 1
+        except rbx.exceptions.NotFound:
+            log.info(f"Tried deleting invalid key (this is normal): {keyName}")
+
+    return clearedKeys
+
+totalClears = 0
+totalDataStores = 0
 
 for game in automateList:
     gameName: str = game["Name"]
     universeId: int = game["UniverseID"]
     targetDatastores = game["Datastores"]
+    print("\n===========\n")
     print(f"Friendly game name: {gameName}")
     print(f"Universe id: {universeId}")
 
     for datastore in targetDatastores:
+        totalDataStores += 1
+
         dataStoreName = str(datastore["Name"])
         targetDeleteKey = str(datastore["KeyTemplate"].replace("{uid}", str(UID)))
-        print(f"Datastore {dataStoreName} | Target key deletion: {targetDeleteKey}")
-        
+        if "*" in targetDeleteKey:
+            partIneed = targetDeleteKey.split("*")[0]
+            print(f"All keys that start with {partIneed} inside {dataStoreName}")
+        else:     
+            print(f"Datastore {dataStoreName} | Target key deletion: {targetDeleteKey}")
+
     confirmDestructiveAction()
 
     clearedKeys = 0
@@ -68,7 +128,20 @@ for game in automateList:
     for datastore in targetDatastores:
         dataStoreName = str(datastore["Name"])
         targetDeleteKey = str(datastore["KeyTemplate"].replace("{uid}", str(UID)))
-        clearKey(universeId, dataStoreName, targetDeleteKey)
-        clearedKeys += 1
+        clearedKeys += clearKey(universeId, dataStoreName, targetDeleteKey)
 
-    print(f"Cleared {clearedKeys} keys in total among {len(targetDatastores)} datastores in a total of {len(automateList)} universes.")
+    print(f"Cleared {clearedKeys} keys in total among {len(targetDatastores)} datastores.")
+
+    totalClears += clearedKeys
+
+Report: str = f"""
+
+\n\n
+Clearing done, stats:
+Cleared a total of {totalClears} among {totalDataStores},
+With a total of {len(automateList)} universes.
+\n\n
+
+(The program will now exit)
+
+"""
